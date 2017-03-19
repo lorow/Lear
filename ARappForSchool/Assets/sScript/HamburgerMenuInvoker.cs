dcbb3c36eaf6711219d0f75e786cf30e51c8a9da
lorow <smykupyka@gmail.com>
2017-03-19 17:29:25 +0100

    added hamburgermenu, still needs some work and love

---
+++ ARappForSchool/Assets/sScript/HamburgerMenuInvoker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HamburgerMenuInvoker : MonoBehaviour
+{
+    public GameObject hamburgerMenu;
+    public bool isActive = false;
+    
+    public void handleMenu()
+    {
+        isActive = !isActive;
+    }
+    private void Update()
+    {
+        hamburgerMenu.SetActive(isActive);
+    }
+}
